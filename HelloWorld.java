49dd811e5a0014957f70cf98602f861d1534e58b
ConnorCao <connorcao710@hotmail.com>
2017-04-21 16:11:18 +0800

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,6 @@ class HelloWorldDemo
 {
 	public static void main(String[] args) 
 	{
-		System.out.println("Hello World!!!!!!!!!!!!!!!!!!");
+		System.out.println("Hello World!!!!!!!!!!!!!!!!!!1321654321");
 	}
 }
